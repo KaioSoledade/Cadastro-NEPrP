1423852091140c6734f8c0d6fdfa26f879660fbd
Kaio Soledade <soledadekaio@gmail.com>
2023-11-30 11:01:37 -0300

    INICIO

--- script.js
+++ script.js
@@ -27,3 +27,32 @@ function emailValidate() {
 
     return isValid; // Retorna o valor de isValid
 }
+
+
+const campoNome = document.getElementById('name');
+const spanErro1 = document.getElementById('span-name');
+
+function setError() {
+    campoNome.style.border = '4px solid #e63636';
+    spanErro1.classList.remove('span-none');
+}
+
+function removeError() {
+    campoNome.style.border = '';
+    spanErro1.classList.add('span-none');
+}
+
+campoNome.addEventListener('input', nameValidate);
+
+function nameValidate() {
+    const input = campoNome.value.trim(); // Remover espaços extras no início e no final do texto
+    const isValidLength = input.length >= 2; // Verificar se o comprimento é maior ou igual a 2
+
+    if (!isValidLength) {
+        setError();
+    } else {
+        removeError();
+    }
+
+    return isValidLength; // Retorna true apenas se o comprimento for maior ou igual a 2
+}
