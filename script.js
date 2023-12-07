fcd1ed0263f5e34a8b20de83673a0ada365e6601
Kaio Soledade <soledadekaio@gmail.com>
2023-12-07 17:02:08 -0300

    Update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const campoEmail = document.getElementById('email');
 const spanErro = document.getElementById('span');
 
 function setError() {
-    campoEmail.style.border = '4px solid #e63636';
+    campoEmail.style.border = '2px solid #e63636';
     spanErro.classList.remove('span-none');
 }
 
@@ -13,6 +13,14 @@ function removeError() {
 }
 
 campoEmail.addEventListener('input', emailValidate);
+campoEmail.addEventListener('input', vazio1);
+
+function vazio1(){
+    if(campoEmail == ''){
+        removeError();
+    }
+}
+
 
 function emailValidate() {
     const input = campoEmail.value;
@@ -24,6 +32,9 @@ function emailValidate() {
     } else {
         removeError();
     }
+    if(campoEmail == ''){
+        removeError();
+    }
 
     return isValid; // Retorna o valor de isValid
 }
@@ -32,26 +43,70 @@ function emailValidate() {
 const campoNome = document.getElementById('name');
 const spanErro1 = document.getElementById('span-name');
 
-function setError() {
-    campoNome.style.border = '4px solid #e63636';
+function setError1() {
+    campoNome.style.border = '2px solid #e63636';
     spanErro1.classList.remove('span-none');
 }
 
-function removeError() {
+function removeError1() {
     campoNome.style.border = '';
     spanErro1.classList.add('span-none');
 }
 
 campoNome.addEventListener('input', nameValidate);
+campoNome.addEventListener('input', vazio2);
+
+function vazio2(){
+
+}
 
 function nameValidate() {
     const input = campoNome.value.trim(); // Remover espaços extras no início e no final do texto
     const isValidLength = input.length >= 2; // Verificar se o comprimento é maior ou igual a 2
 
     if (!isValidLength) {
-        setError();
+        setError1();
     } else {
-        removeError();
+        removeError1();
+    }
+    if(campoNome == ""){
+        removeError1();
+    }
+
+    return isValidLength; // Retorna true apenas se o comprimento for maior ou igual a 2
+}
+
+
+
+
+const senha1 = document.getElementById('pass1');
+const senha2 = document.getElementById('pass2');
+
+const spanErro2 = document.getElementById('span-pass1');
+const spanErro3 = document.getElementById('span-pass2');
+
+
+function setError2() {
+    senha1.style.border = '2px solid #e63636';
+    senha2.style.border = '2px solid #e63636';
+    spanErro3.classList.remove('span-none');
+}
+
+function removeError2() {
+    senha1.style.border = '';
+    senha2.style.border = '';
+    spanErro3.classList.add('span-none');
+}
+
+senha2.addEventListener('input', senhaValidat);
+
+function senhaValidat() {
+    const senhaIgual = senha1.value == senha2.value; // Verificar se o comprimento é maior ou igual a 2
+
+    if (!senhaIgual) {
+        setError2();
+    } else {
+        removeError2();
     }
 
     return isValidLength; // Retorna true apenas se o comprimento for maior ou igual a 2
